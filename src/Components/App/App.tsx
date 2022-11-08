2e48b7dc4453cff9561aac02ccf622248c8e3df9
enzo <Eynzof@gmail.com>
2022-11-08 10:51:29 +0800

    13-28

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -7,7 +7,10 @@ function App() {
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
+          {/*<SearchBar />*/}
           <div className="App-playlist">
+              {/*<SearchResults />*/}
+              {/*<PlayList />*/}
           </div>
         </div>
       </div>
